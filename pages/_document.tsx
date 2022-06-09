571da869be1282385a485cadd20d83ad318c1912
zarnimy0 <zarni@getgo.sg>
2022-06-09 18:46:34 +0800

    Solution updated

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        {/* <script
+          src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+          async
+        /> */}
+      </body>
+    </Html>
+  );
+}
